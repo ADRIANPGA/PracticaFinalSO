ee1a1f55688d05933fbe8797b4c7710985e49346
AdrianPerezGarcia <apereg24@estudiantes.unileon.es>
2019-12-05 16:24:05 +0100

    Optimizada la entrada de parametros

--- PracticaFinal.c
+++ PracticaFinal.c
@@ -12,25 +12,21 @@ int main(int argc, char const *argv[]) {
 	printf("Funka esto.\n");
 
 	/* Switch de comprobacion de los argumentos iniciales (Parte extra de parametros estáticos) */
-	int numSolicitudes, numAtendedores
+	int numSolicitudes = 15, numAtendedores = 1;
 	switch(argc){
 		case 1:
 			printf("No se han introducido parametros de entrada.\n");
 			printf("Se inicializaran al valor por defecto (15 solicitudes y 1 atendedorPRO).\n\n");
-			numSolicitudes = 15;
-			numAtendedores = 15;
 		break;
 		case 2:
             printf("Se ha introducido el valor de las solicitudes, ");
             if (atoi(argv[1]) < 1){
             	printf("pero no era valido, se introdujo un %d pero el valor desde de ser mayor que 1.\n", atoi(argv[1]));
             	printf("Se inicializara al valor por defecto (15 solicitudes).\n\n");
-            	numSolicitudes = 15;
             } else{
             	printf("valor cambiado a %d.\n\n", atoi(argv[1]));
             	numSolicitudes = atoi(argv[1]);
             }
-            numAtendedores = 1;
         break;
         case 3:
         	printf("Se han introducido los dos valores.\n");
@@ -39,7 +35,6 @@ int main(int argc, char const *argv[]) {
          	if (atoi(argv[1]) < 1){
             	printf("no es valido, se introdujo un %d pero el valor desde de ser mayor que 1.\n", atoi(argv[1]));
             	printf("Se inicializara al valor por defecto (15 solicitudes).\n\n");
-            	numSolicitudes = 15;
             } else{
             	printf("valor cambiado a %d.\n\n", atoi(argv[1]));
             	numSolicitudes = atoi(argv[1]);
@@ -49,7 +44,6 @@ int main(int argc, char const *argv[]) {
             if (atoi(argv[2]) < 1){
             	printf("no es valido, se introdujo un %d pero el valor desde de ser mayor que 1.\n", atoi(argv[2]));
             	printf("Se inicializara al valor por defecto (1 atendedor).\n\n");
-            	numAtendedores = 15;
             } else{
             	printf("valor cambiado a %d.\n\n", atoi(argv[2]));
             	numAtendedores = atoi(argv[2]);
@@ -57,8 +51,6 @@ int main(int argc, char const *argv[]) {
         break;
         default:
         	printf("Error en los parametros de entrada (%d de 2) : Se inicializaran los parametros a los valores por defecto (15 solicitudes y 1 atendedor).\n", argc-1);
-        	numSolicitudes = 15;
-        	numAtendedores = 1;
         break;
 	}
 
